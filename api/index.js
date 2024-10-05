d8ad1dafeb771a31b53c6dbeb1976c14f93f5f13
NAGARAJS2002 <nr2277644@gmail.com>
2024-10-05 09:02:15 +0530

    add mongoDB database

--- api/index.js
+++ api/index.js
@@ -1,12 +1,18 @@
 import express from "express"
+import env from "dotenv"
+import mongoose from "mongoose";
+env.config();
 const app = express();
 const port = 3000;
 
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log(`database connected`);
+    
+})
 
-
-
+ 
 
 app.listen(port ,()=>{
-    console.log(`server running on port ${port}`);
+    console.log(`server is running on port ${port}`);
     
 })
